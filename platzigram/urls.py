0a3e9e8f889d4cf2c9b9ba6d35322b92049940d1
Alberto Ramírez <alberto.r.caballero.87@gmail.com>
2019-06-29 09:10:34 +0200

    Clase 18

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 from platzigram import views as local_views
 
@@ -26,4 +27,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
 
     path('posts/', posts_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
