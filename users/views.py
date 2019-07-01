358026e220b649100a20c2dcb922e52d7c836129
albertorc87 <alberto.r.caballero.87@gmail.com>
2019-07-01 16:39:54 +0200

    Continuación clase 20

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
+"""Users views."""
+
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_view(request):
+	"""Login view."""
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+	return render(request, 'users/login.html')
